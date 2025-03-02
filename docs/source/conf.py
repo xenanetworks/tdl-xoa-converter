9c78f111e61365555f1b73c299b1c302f4aaa483
Leonard Yu <leonard.yu@teledyne.com>
2025-03-02 21:29:18 +0100

    Update doc gen with new icons

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -123,27 +123,12 @@ html_title = title
 # The path to the HTML logo image in the static path, or URL to the logo, or ''.
 # html_logo = './_static/xoa_logo.png'
 
-html_favicon = './_static/xoa_favicon_16.png'
+html_favicon = './_static/favicon.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-# Theme config for Furo
-# html_show_copyright = True
-# html_show_sphinx = False
-# html_theme_options = {
-#     "light_logo": "xoa_logo_light.png",
-#     "dark_logo": "xoa_logo_dark.png",
-#     "source_repository": "https://github.com/xenanetworks/open-automation-config-converter",
-#     "light_css_variables": {
-#         "color-brand-primary": "#295341",
-#         "color-brand-content": "#295341",
-#     },
-#     "navigation_with_keys": True,
-# }
-# html_split_index = True
 
 # Theme config for sphinx_rtd_theme
 html_show_sphinx =  False
